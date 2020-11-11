725a14c17473b19d72772d4ebceda75deecacfe2
Jerry U <jerry@lyshnia.com>
2020-11-11 10:22:24 +0800

    Added Dashboard

---
+++ frontend/views/dashboard-view.js
@@ -0,0 +1,52 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+
+class DashboardView extends PolymerElement {
+
+    static get template() {
+        return html`
+<style include="shared-styles">
+                :host {
+                    display: block;
+                    height: 100%;
+                }
+            </style>
+<vaadin-vertical-layout style="width: 100%; overflow: auto;">
+ <vaadin-horizontal-layout theme="spacing" style="width: 100%; margin-top: var(--lumo-space-l); align-items: center; padding-right: var(--lumo-space-s); padding-left: var(--lumo-space-s);">
+  <img style="align-self: flex-end; width: 40px; height: 40px;" src="icons/user.png">
+  <img style="height: 40px; flex-grow: 0; flex-shrink: 1; margin-left: 24%;" src="images/logo.png">
+ </vaadin-horizontal-layout>
+ <vaadin-vertical-layout theme="spacing" style="width: 100%; margin-top: var(--lumo-space-s); padding-left: var(--lumo-space-s); padding-right: var(--lumo-space-s);">
+  <vaadin-horizontal-layout theme="spacing-xs" style="width: 100%;  font-size: x-large;">
+   <label>Hello, </label>
+   <label style="font-weight: bold;">User!</label>
+  </vaadin-horizontal-layout>
+  <label style="color: gray; margin-top: 0px;">Good Morning, welcome back.</label>
+ </vaadin-vertical-layout>
+ <vaadin-horizontal-layout theme="spacing-s" id="servicesLayout" style="width: 100%; margin-top: var(--lumo-space-xl); overflow: scroll;"></vaadin-horizontal-layout>
+ <label style="margin-top: var(--lumo-space-xl); margin-left: var(--lumo-space-s);">Featured Post</label>
+ <vaadin-horizontal-layout theme="spacing-s" id="fpostLayout" style="width: 100%; margin-right: var(--lumo-space-xs); overflow: auto;"></vaadin-horizontal-layout>
+ <vaadin-horizontal-layout style="align-items: center; justify-content: space-between; margin-left: var(--lumo-space-s); width: 100%; margin-right: var(--lumo-space-xs); margin-top: var(--lumo-space-m); padding-right: var(--lumo-space-m);">
+  <label style="margin: 0px;">Featured Dealers</label>
+  <a href="https://vaadin.com" style="text-decoration: none; color: var(--lumo-primary-color);">See All</a>
+ </vaadin-horizontal-layout>
+ <vaadin-horizontal-layout theme="spacing-s" id="fdealersLayout" style="width: 100%; margin-right: var(--lumo-space-xs); overflow: auto; margin-left: var(--lumo-space-m); height: 200px;"></vaadin-horizontal-layout>
+ <label style="margin-top: var(--lumo-space-xl); margin-left: var(--lumo-space-s);">Fast Deall</label>
+ <vaadin-horizontal-layout theme="spacing-s" id="fdealLayout" style="width: 100%; margin-right: var(--lumo-space-xs); overflow: auto;"></vaadin-horizontal-layout>
+</vaadin-vertical-layout>
+`;
+    }
+
+    static get is() {
+        return 'dashboard-view';
+    }
+
+    static get properties() {
+        return {
+            // Declare your properties here.
+        };
+    }
+}
+
+customElements.define(DashboardView.is, DashboardView);
---
+++ src/main/java/com/lyshnia/pcari/app/DashboardView.java
@@ -0,0 +1,182 @@
+package com.lyshnia.pcari.app;
+
+import com.github.appreciated.card.ClickableCard;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.templatemodel.TemplateModel;
+
+/**
+ * A Designer generated component for the dashboard-view template.
+ * <p>
+ * Designer will add and remove fields with @Id mappings but
+ * does not overwrite or otherwise change this file.
+ */
+@Tag("dashboard-view")
+@JsModule("./views/dashboard-view.js")
+public class DashboardView extends PolymerTemplate<DashboardView.DashboardViewModel> {
+
+    @Id("servicesLayout")
+    private HorizontalLayout servicesLayout;
+    @Id("fdealersLayout")
+    private HorizontalLayout fdealersLayout;
+    @Id("fpostLayout")
+    private HorizontalLayout fpostLayout;
+    @Id("fdealLayout")
+    private HorizontalLayout fdealLayout;
+
+    /**
+     * Creates a new DashboardView.
+     */
+    public DashboardView() {
+        // You can initialise any data required for the connected UI components here.
+        servicesLayout.add(createServiceCard("Car", "icons/car.svg"));
+        servicesLayout.add(createServiceCard("Motorcycle", "icons/cyc.svg"));
+        servicesLayout.add(createServiceCard("Commercial", "icons/com.svg"));
+        servicesLayout.add(createServiceCard("Services", "icons/ser.svg"));
+
+        fpostLayout.add(createFPCard("MINI COOPER S", "images/cooper.jpg"));
+        fpostLayout.add(createFPCard("MITSUBISHI ASX 2.0 4AWD FACELIFT (AT)", "images/asx.jpg"));
+        fpostLayout.add(createFPCard("MITSUBISHI ASX 2.0 4AWD FACELIFT (AT)", "images/asx.jpg"));
+
+
+        fdealersLayout.add(createFDCard("Utama Motors", "images/asx.jpg", 1l));
+        fdealersLayout.add(createFDCard("Kenghim Motors", "images/asx.jpg", 1l));
+        fdealersLayout.add(createFDCard("Utama Motors", "images/asx.jpg", 1l));
+        fdealersLayout.add(createFDCard("Kenghim Motors", "images/asx.jpg", 1l));
+        fdealersLayout.add(createFDCard("Utama Motors", "images/asx.jpg", 1l));
+        fdealersLayout.add(createFDCard("Kenghim Motors", "images/asx.jpg", 1l));
+        fdealersLayout.add(createFDCard("Utama Motors", "images/asx.jpg", 1l));
+        fdealersLayout.add(createFDCard("Kenghim Motors", "images/asx.jpg", 1l));
+
+        fdealLayout.add(createFPCard("MINI COOPER S", "images/cooper.jpg"));
+        fdealLayout.add(createFPCard("MITSUBISHI ASX 2.0 4AWD FACELIFT (AT)", "images/asx.jpg"));
+        fdealLayout.add(createFPCard("MITSUBISHI ASX 2.0 4AWD FACELIFT (AT)", "images/asx.jpg"));
+    }
+
+    public Component createServiceCard(String service, String img) {
+        VerticalLayout vl = new VerticalLayout();
+        ClickableCard card = new ClickableCard();
+        card.setHeight("80px");
+        card.setWidth("80px");
+        card.getChildren().findFirst().get().getElement().getStyle().set("background-color", "white");
+        ((VerticalLayout) card.getChildren().findFirst().get()).setAlignItems(FlexComponent.Alignment.CENTER);
+        ((VerticalLayout) card.getChildren().findFirst().get()).setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
+        Image image = new Image(img, service);
+        image.setWidth("70px");
+        card.add(image);
+
+        Label label = new Label(service);
+        label.getStyle().set("font-size", "small");
+
+        vl.add(card, label);
+        vl.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        vl.setAlignItems(FlexComponent.Alignment.CENTER);
+        vl.setSpacing(false);
+        vl.setPadding(false);
+        vl.getStyle().set("margin", "0px");
+
+        return vl;
+    }
+
+    public Component createFPCard(String service, String img) {
+        ClickableCard card = new ClickableCard();
+        card.setHeight("110px");
+        card.setWidth("200px");
+        card.setMinWidth("200px");
+        card.getChildren().findFirst().get().getElement().getStyle().set("background-image", "url(" + img + ")");
+
+        Label label = new Label(service);
+        label.getStyle().set("font-size", "10px");
+        label.getStyle().set("margin-left", "23px");
+
+        HorizontalLayout hl = new HorizontalLayout(label);
+        hl.getStyle().set("background-color", "rgba(192,192,192,0.7)");
+        hl.setWidthFull();
+        hl.setHeight("32px");
+        hl.setAlignItems(FlexComponent.Alignment.CENTER);
+
+
+        ((VerticalLayout) card.getChildren().findFirst().get()).setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+
+        card.add(hl);
+
+        return card;
+    }
+
+    public Component createFDCard(String dealer, String logo, Long id) {
+        ClickableCard card = new ClickableCard();
+        card.setWidth("120px");
+        card.setMinWidth("120px");
+        card.setHeight("190px");
+
+
+        Image image = new Image(logo, dealer);
+        image.setWidth("70px");
+        image.setHeight("70px");
+        image.getStyle().set("border-radius", "50%");
+
+
+        Label label = new Label(dealer);
+        label.getStyle().set("font-size", "13px");
+        label.getStyle().set("font-weight", "bold");
+        label.getStyle().set("padding-left", "5px");
+        label.getStyle().set("padding-top", "13px");
+
+        Span span = new Span("43 Total Ads");
+        span.getStyle().set("font-size", "10px");
+        span.getStyle().set("padding-left", "5px");
+        span.getStyle().set("color", "gray");
+
+
+//        ((VerticalLayout) card.getChildren().findFirst().get()).getStyle().set("padding-left", "10px");
+
+        HorizontalLayout imageLayout = new HorizontalLayout(image);
+        imageLayout.setWidthFull();
+        imageLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
+        HorizontalLayout lastLayout = new HorizontalLayout();
+        Icon star = VaadinIcon.STAR.create();
+        star.getStyle().set("width", "10px");
+        star.getStyle().set("padding-left", "5px");
+
+        Span span2 = new Span("4.7");
+        span2.getStyle().set("font-size", "10px");
+        span2.getStyle().set("padding-left", "2px");
+        lastLayout.add(star);
+        lastLayout.add(span2);
+
+        lastLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        lastLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        lastLayout.setSpacing(false);
+
+
+        Button button = new Button("Profile");
+        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_SMALL);
+        button.getStyle().set("align-self", "center");
+
+        card.add(imageLayout, label, span, lastLayout, button);
+
+        return card;
+    }
+
+    /**
+     * This model binds properties between DashboardView and dashboard-view
+     */
+    public interface DashboardViewModel extends TemplateModel {
+        // Add setters and getters for template properties here.
+    }
+}
